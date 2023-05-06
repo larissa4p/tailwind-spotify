370fe86e39916f3055ccc95204ff646520c83104
larisbessa <larissabessa4@gmail.com>
2023-05-06 14:10:10 -0300

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
-import { Home as HomeIcon, Search, Library } from "lucide-react";
+import {
+  Home as HomeIcon,
+  Search,
+  Library,
+  ChevronLeft,
+  ChevronRight,
+  Play,
+} from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -36,16 +44,214 @@ export default function Home() {
           </nav>
 
           <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-2">
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Músicas Curtidas</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Lo-Fi hip-hop</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Blues Instrumentals Only</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Heavy Instrumental Metal</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Jazz Relax</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Blues Focus</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Top Brasil</a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Músicas Curtidas
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Lo-Fi hip-hop
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Blues Instrumentals Only
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Heavy Instrumental Metal
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Jazz Relax
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Blues Focus
+            </a>
+            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">
+              Top Brasil
+            </a>
           </nav>
         </aside>
-        <main className="flex-1 p-6">main</main>
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-1">
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Metallica"
+              />
+              <strong>Black Album</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-1 rounded-md-full bg-green-400 
+              text-black ml-auto mr-8 invisible group-hover:visible"
+              >
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/10 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Metallica"
+              />
+              <strong>Black Album</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 
+              text-black ml-auto mr-8 invisible group-hover:visible"
+              >
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Metallica"
+              />
+              <strong>Black Album</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 
+              text-black ml-auto mr-8 invisible group-hover:visible"
+              >
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Metallica"
+              />
+              <strong>Black Album</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 
+              text-black ml-auto mr-8 invisible group-hover:visible"
+              >
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Metallica"
+              />
+              <strong>Black Album</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 
+              text-black ml-auto mr-8 invisible group-hover:visible"
+              >
+                <Play />
+              </button>
+            </a>
+            <a
+              href="#"
+              className="bg-white/5 group rounded-md flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors"
+            >
+              <Image
+                src="/album.jpg"
+                width={104}
+                height={104}
+                alt="Capa do álbum Metallica"
+              />
+              <strong>Black Album</strong>
+              <button
+                className="w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 
+              text-black ml-auto mr-8 invisible group-hover:visible"
+              >
+                <Play />
+              </button>
+            </a>
+          </div>
+
+          <h2 className="font-semibold text-2xl mt-10">
+            Made for Larissa Bessa
+          </h2>
+
+          <div className="grid grid-cols-8 gap-4 mt-4">
+            <a href="##" className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10">
+              <Image
+                className="w-full"
+                src="/album.jpg"
+                width={120}
+                height={120}
+                alt="Capa do álbum Metallica"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
+            </a>
+            <a href="##" className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10">
+              <Image
+                className="w-full"
+                src="/album.jpg"
+                width={120}
+                height={120}
+                alt="Capa do álbum Metallica"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
+            </a>
+            <a href="##" className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10">
+              <Image
+                className="w-full"
+                src="/album.jpg"
+                width={120}
+                height={120}
+                alt="Capa do álbum Metallica"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
+            </a>
+            <a href="##" className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10">
+              <Image
+                className="w-full"
+                src="/album.jpg"
+                width={120}
+                height={120}
+                alt="Capa do álbum Metallica"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
+            </a>
+            <a href="##" className="bg-white/5 p-3 flex flex-col gap-2 rounded-md hover:bg-white/10">
+              <Image
+                className="w-full"
+                src="/album.jpg"
+                width={120}
+                height={120}
+                alt="Capa do álbum Metallica"
+              />
+              <strong className="font-semibold">Daily Mix 1</strong>
+              <span className="text-sm text-zinc-400">Wallows, COIN, girl in red and more</span>
+            </a>
+          </div>
+        </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
         footer
